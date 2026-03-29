2da51e85f183e45e4028b984da834ad86bf9ff93
Oleksii Oleksiichuk <frodo.work@gmail.com>
2026-03-29 18:32:49 +0300

    fix logger

--- stalecheck/logger.py
+++ stalecheck/logger.py
@@ -8,14 +8,19 @@ def setup_logger(verbose: bool = False) -> logging.Logger:
     logger = logging.getLogger("stalecheck")
     logger.setLevel(level)
 
-    handler = logging.StreamHandler(sys.stderr)
-    handler.setLevel(level)
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stderr)
+        handler.setLevel(level)
 
-    formatter = logging.Formatter(
-        fmt="%(asctime)s - [%(levelname)s] - %(message)s",
-        datefmt="%H:%M:%S",
-    )
-    handler.setFormatter(formatter)
+        formatter = logging.Formatter(
+            fmt="%(asctime)s - [%(levelname)s] - %(message)s",
+            datefmt="%H:%M:%S",
+        )
+        handler.setFormatter(formatter)
 
-    logger.addHandler(handler)
+        logger.addHandler(handler)
+    else:
+        # If the level was changed during a second call, update it.
+        logger.handlers[0].setLevel(level)
+
     return logger
