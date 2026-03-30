ee3b39898d3b755cc9a3cb0641c245d0152894ea
Oleksii Oleksiichuk <frodo.work@gmail.com>
2026-03-31 00:04:54 +0300

    write basic CLI entrypoint for test in `cli.py`

--- stalecheck/cli.py
+++ stalecheck/cli.py
@@ -1,10 +1,22 @@
+from pathlib import Path
+
 import click
 
+from stalecheck.checker import Checker
+from stalecheck.logger import setup_logger
+from stalecheck.output import export_json, print_table
 
-@click.group()
-@click.pass_context
-def stalecheck(ctx):
-    """
-    Test help
-    """
-    ctx.obj = ...
+
+@click.command()
+@click.option("--file", "-f", type=click.Path(exists=True))
+@click.option("--export", type=click.Path())
+@click.option("--verbose", "-v", is_flag=True)
+def stalecheck(file, export, verbose):
+    setup_logger(verbose)
+
+    results = Checker().run(Path(file) if file else None)
+
+    if export:
+        export_json(results, Path(export))
+    else:
+        print_table(results)
